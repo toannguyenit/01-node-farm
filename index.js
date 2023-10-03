bc8c459b7b366b0f5b37209b7f9d2b30d72848ae
toannguyenit <toannguyen.hcmute@gmail.com>
2023-10-03 17:01:14 +0700

    Using module: Our Own Modules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
+const replaceTemplate = require('./modules/replaceTemplate')
 // Blocking
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
 // console.log(textIn);
@@ -21,21 +21,7 @@ const url = require("url");
 // SERVER
 // const __dirname = ".";
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
 
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-
-  return output;
-};
 
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template_overview.html`,
